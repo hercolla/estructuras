3d696c7f9308a1bd96afc10831eccd535caafb2d
hercolla <jhernandezcollavino@gmail.com>
2019-04-26 20:09:46 -0500

    Update estructura2.php

--- estructura2.php
+++ estructura2.php
@@ -1,4 +1,16 @@
-<?php  
+<?php 
+$monto;//indicar si el monto tiene descuento   (monto>1000) 
+$edad;//indicar si es mayor o menor de edad
+$sueldo//indicar si el sueldo es minimo o no
+
+
+if (monto) 
+{
+	echo "descuento de mayor a 1000";
+	
+}
+
+
 	if (true)
 	{
 	echo "mensaje por ser true";
@@ -6,6 +18,8 @@
 	{
 	echo "mensaje por ser false";
 	}
+	echo ">br />";
+	echo false?"mensaje por ser true":"mensaje por ser false"
 
 
 ?>
